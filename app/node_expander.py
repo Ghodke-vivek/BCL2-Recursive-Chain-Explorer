16b388ed3e8788450bcad592c9f2917a94c35d7a
Ghodke-vivek <vivekghodke06@gmail.com>
2026-05-12 12:42:16 +0530

    Update node_expander.py

--- app/node_expander.py
+++ app/node_expander.py
@@ -1,1 +1,9 @@
+def get_connected_nodes(G, node_id):
 
+    incoming = list(G.predecessors(node_id))
+    outgoing = list(G.successors(node_id))
+
+    return {
+        "incoming": incoming,
+        "outgoing": outgoing,
+    }
