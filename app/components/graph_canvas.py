3d3678fdc4e56e2eb33eae39d6ac0e02cb24560e
Ghodke-vivek <vivekghodke06@gmail.com>
2026-05-12 23:57:33 +0530

    Update graph_canvas.py

--- app/components/graph_canvas.py
+++ app/components/graph_canvas.py
@@ -1,10 +1,25 @@
 from streamlit_agraph import agraph
 
+# =========================================================
+# GRAPH RENDERER
+# =========================================================
 
-def render_graph(nodes, edges, config):
+def render_graph(
 
-    return agraph(
+    nodes,
+
+    edges,
+
+    config
+):
+
+    selected_node = agraph(
+
         nodes=nodes,
+
         edges=edges,
+
         config=config
     )
+
+    return selected_node
