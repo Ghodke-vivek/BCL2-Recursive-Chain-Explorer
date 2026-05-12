8140fe361c6006eb58b7ed234ba73cfa48c7803b
Ghodke-vivek <vivekghodke06@gmail.com>
2026-05-12 12:41:44 +0530

    Update feedback_detector.py

--- app/feedback_detector.py
+++ app/feedback_detector.py
@@ -1,1 +1,35 @@
+import networkx as nx
 
+def detect_feedback_paths(G, seed_nodes):
+
+    feedback_edges = []
+
+    for node in G.nodes:
+
+        for seed_node in seed_nodes:
+
+            if node == seed_node:
+                continue
+
+            try:
+
+                path = nx.shortest_path(
+                    G,
+                    source=node,
+                    target=seed_node,
+                )
+
+                if len(path) > 1:
+
+                    feedback_edges.append(
+                        {
+                            "source": node,
+                            "target": seed_node,
+                            "path": path,
+                        }
+                    )
+
+            except:
+                pass
+
+    return feedback_edges
