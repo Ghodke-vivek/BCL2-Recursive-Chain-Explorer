8ea4176a65608a557cc6719f3fd6698503b6f307
Ghodke-vivek <vivekghodke06@gmail.com>
2026-05-12 12:40:07 +0530

    Update pathway_manager.py

--- app/pathway_manager.py
+++ app/pathway_manager.py
@@ -1,1 +1,15 @@
+import random
 
+
+def generate_color():
+    return "#%06x" % random.randint(0, 0xFFFFFF)
+
+
+def build_pathway_colors(pathways):
+
+    pathway_colors = {}
+
+    for pathway in pathways:
+        pathway_colors[pathway] = generate_color()
+
+    return pathway_colors
