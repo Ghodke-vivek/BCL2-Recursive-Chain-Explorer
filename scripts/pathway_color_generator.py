d6db7766de85f0ecbb625adad8e98d157a13d8aa
Ghodke-vivek <vivekghodke06@gmail.com>
2026-05-12 13:12:55 +0530

    Update pathway_color_generator.py

--- scripts/pathway_color_generator.py
+++ scripts/pathway_color_generator.py
@@ -1,1 +1,20 @@
+import random
+import json
 
+pathways = [
+    "Neurotrophin signaling pathway",
+    "PI3K-Akt signaling pathway",
+    "Apoptosis"
+]
+
+colors = {}
+
+for pathway in pathways:
+
+    colors[pathway] = "#%06x" % random.randint(0, 0xFFFFFF)
+
+with open("../app/assets/pathway_colors.json", "w") as f:
+
+    json.dump(colors, f, indent=4)
+
+print("Pathway colors generated.")
