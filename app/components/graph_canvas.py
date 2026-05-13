69742ae1d6a3a49382c3705739eeb7afdbe68b12
Ghodke-vivek <vivekghodke06@gmail.com>
2026-05-13 13:11:43 +0530

    Update graph_canvas.py

--- app/components/graph_canvas.py
+++ app/components/graph_canvas.py
@@ -4,22 +4,16 @@ from streamlit_agraph import agraph
 # GRAPH RENDERER
 # =========================================================
 
-def render_graph(
-
-    nodes,
-
-    edges,
-
-    config
-):
+def render_graph(nodes, edges, config):
 
     selected_node = agraph(
-
         nodes=nodes,
-
         edges=edges,
-
         config=config
     )
+
+    # ensure string output
+    if selected_node is not None:
+        selected_node = str(selected_node)
 
     return selected_node
